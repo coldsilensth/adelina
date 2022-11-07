562f3de8685aef9a3494ab01e4890256001d9c3d
Adeline <k4adel2020@gmail.com>
2022-11-07 19:54:27 +0600

    lokal report

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("я закометил новый проект");
+        System.out.println("Строка для локального репозитори");
     }
 }
